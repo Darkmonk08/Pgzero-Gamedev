12aeded59b8445dc9e5359140855c3d867d669de
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-01-21 19:50:15 +0530

    bumblebee and the flower.py

--- bumblebee and the flower.py
+++ bumblebee and the flower.py
@@ -4,6 +4,7 @@ import random
 TITLE="BUMBLEBEE VS THE FLOWER"
 HEIGHT=500
 WIDTH=600
+gameover=False
 
 bee=Actor("bee")
 bee.pos=(random.randint(50,WIDTH-50), random.randint(50,HEIGHT-50))
@@ -12,10 +13,19 @@ flower.pos=(random.randint(50,WIDTH-50), random.randint(50,HEIGHT-50))
 score=0
  #screen.draw.text("Shoot the alien", center=(WIDTH/2, 40), fontsize=40, color="black")
 def draw():
-    screen.blit("background", (0,0))
-    bee.draw()
-    flower.draw()
-    screen.draw.text("Score:"+str(score),(1,1), fontsize=30, color="black")
+    if gameover==False:
+        screen.blit("background", (0,0))
+        bee.draw()
+        flower.draw()
+        screen.draw.text("Score:"+str(score),(1,1), fontsize=30, color="black")
+    else:
+        screen.fill("Blue")
+        screen.draw.text("Game over, your score was "+str(score),(10,10), fontsize=60, color="black")
+        
+
+def gameend():
+    global gameover
+    gameover=True
 
 
 '''def on_key_down(key):
@@ -37,7 +47,7 @@ def update():
         flower.pos=(random.randint(50,WIDTH-50), random.randint(50,HEIGHT-50))
         score=score+1
     
-
+clock.schedule(gameend, 30)
 
 
 
