53d0e55ef78dedef0c34c6b1baa4afd08be4c878
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-02-11 20:28:05 +0530

    file handling.py

--- file handling.py
+++ file handling.py
@@ -12,6 +12,9 @@ qbox3=Rect(365,330,345,200)
 qbox4=Rect(365,540,345,200)
 sbox=Rect(720,330,270, 410)
 options=[qbox1,qbox2,qbox3,qbox4]
+totalquestions=0
+questionnum=0
+question=""
 
 def draw():
     screen.fill("Black")
@@ -22,19 +25,45 @@ def draw():
     for option in options:
         screen.draw.filled_rect(option,"yellow") 
     
-    screen.draw.textbox("Welcome to the Quiz! This is Q_ of _", marqueebox)
+    screen.draw.textbox(f"Welcome to the Quiz! This is Q{questionnum} of {totalquestions}", marqueebox)
+    screen.draw.textbox(f"{question[0].strip()}", qbox)
+    screen.draw.textbox(f"{question[1].strip()}",qbox1)
+    screen.draw.textbox(f"{question[2].strip()}",qbox2)
+    screen.draw.textbox(f"{question[3].strip()}",qbox3)
+    screen.draw.textbox(f"{question[4].strip()}",qbox4)
+    screen.draw.textbox("skip",sbox, angle=90)
                     
 
 def update():
-    pass
+    marqueebox.x=marqueebox.x-1
+    if marqueebox.right<=0:
+        marqueebox.left=WIDTH
 
 
 def readquestfile():
+    global totalquestions
     openfile=open("questions.txt",'r')
     for statement in openfile:
         statements.append(statement)
     openfile.close()
+    totalquestions=len(statements)
+
+def readnextquestion():
+    global questionnum, question
+    questionnum=questionnum+1
+    question=statements.pop(0).split("|")
+    print(question)
+
+def on_mouse_down(pos):
+    if sbox.collidepoint(pos):
+        readnextquestion()
+    for b in options:
+        if b.collidepoint(pos):
+            if options.index(b)==int(question[5])-1:
+                print("correct")
+
+        
 
 readquestfile()
-print(statements)
+readnextquestion()
 pgzrun.go()
