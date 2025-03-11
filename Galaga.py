b8f8ae43df262d64ba135a4b15a85f86f1135716
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-03-11 20:26:06 +0530

    Galaga.py

--- Galaga.py
+++ Galaga.py
@@ -7,18 +7,31 @@ galaga=Actor("galaga")
 galaga.pos=(WIDTH/2,HEIGHT-50)
 bullets=[]
 enemeys=[]
+xpos=25
+ypos=25
+score=0
+direction=1
 for i in range(3):
     row=[]
     for i in range(3):
         enemey=Actor("galagaenemy")
+        enemey.pos=(xpos,ypos)
         row.append(enemey)
+        xpos=xpos+70
     enemeys.append(row)
+    ypos=ypos+50
+    xpos=25
+        
 print(enemeys)
 def draw():
     screen.fill("Black")
     galaga.draw()
     for bullet in bullets:
         bullet.draw()
+    for enemey in enemeys:
+        for i in enemey:
+            i.draw()
+    screen.draw.text("Score:"+str(score),(WIDTH-100,HEIGHT-100), fontsize=30, color="white")
 
 
 
@@ -33,6 +46,8 @@ def on_key_down(key):
 
 
 def update():
+    global score
+    global direction
     if keyboard.right and galaga.x!=WIDTH:
         galaga.x=galaga.x+5
     if keyboard.left and galaga.x!=0:
@@ -42,6 +57,26 @@ def update():
             bullet.y=bullet.y-10
         else:
             bullets.remove(bullet)
+    #checking for enemy and bullet collision
+    for i in range(len(enemeys)): 
+        for enemey in enemeys[i]:
+            for bullet in bullets:
+                if bullet.colliderect(enemey):
+                    enemeys[i].remove(enemey)
+                    bullets.remove(bullet)
+                    score=score+1
+    #moving the enemey
+    movedown=False
+    if enemeys[0][-1].x>=WIDTH or enemeys[0][0].x<=0:
+        direction=direction*-1
+        movedown=True
+    for enemey in enemeys:
+        for i in enemey:
+            i.x=i.x+2*direction
+            if movedown==True:
+                i.y=i.y+50
+    
+
 
 
 
