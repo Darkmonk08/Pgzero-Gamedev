56929a42ab8878bfc4126542bdca08cb0abd8eef
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-03-18 20:29:15 +0530

    Galaga.py

--- Galaga.py
+++ Galaga.py
@@ -11,6 +11,9 @@ xpos=25
 ypos=25
 score=0
 direction=1
+down=False
+gameover=False
+
 for i in range(3):
     row=[]
     for i in range(3):
@@ -24,14 +27,18 @@ for i in range(3):
         
 print(enemeys)
 def draw():
-    screen.fill("Black")
-    galaga.draw()
-    for bullet in bullets:
-        bullet.draw()
-    for enemey in enemeys:
-        for i in enemey:
-            i.draw()
-    screen.draw.text("Score:"+str(score),(WIDTH-100,HEIGHT-100), fontsize=30, color="white")
+    if gameover==False:
+        screen.fill("Black")
+        galaga.draw()
+        for bullet in bullets:
+            bullet.draw()
+        for enemey in enemeys:
+            for i in enemey:
+                i.draw()
+        screen.draw.text("Score:"+str(score),(WIDTH-100,HEIGHT-100), fontsize=30, color="white")
+    else:
+        screen.fill("Red")
+        screen.draw.text("Gamover, your score was1"+str(score),(300,200), fontsize=100, color="black")
 
 
 
@@ -48,6 +55,8 @@ def on_key_down(key):
 def update():
     global score
     global direction
+    global down
+    global gameover
     if keyboard.right and galaga.x!=WIDTH:
         galaga.x=galaga.x+5
     if keyboard.left and galaga.x!=0:
@@ -67,14 +76,37 @@ def update():
                     score=score+1
     #moving the enemey
     movedown=False
-    if enemeys[0][-1].x>=WIDTH or enemeys[0][0].x<=0:
+    if len(enemeys)>0 and (enemeys[0][-1].x>=WIDTH or enemeys[0][0].x<=0):
         direction=direction*-1
         movedown=True
     for enemey in enemeys:
         for i in enemey:
-            i.x=i.x+2*direction
+            i.x=i.x+50*direction
             if movedown==True:
                 i.y=i.y+50
+                down=True
+    if down==True:
+        enemeyrow=[]
+        if direction==1:
+            xposi=0
+        else:
+            xposi=WIDTH
+        yposi=30
+        for i in range(3):
+            newenemey=Actor("galagaenemy")
+            newenemey.pos=(xposi,yposi)
+            xposi=xposi+(70*direction) 
+            enemeyrow.append(newenemey)
+        enemeys.append(enemeyrow)
+        print(enemeys)        
+        down=False
+    #checking for enemey galaga collision
+    for i in range(len(enemeys)):
+        for enemey in enemeys[i]:
+            if galaga.colliderect(enemey):
+                gameover=True
+
+
     
 
 
